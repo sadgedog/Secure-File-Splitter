bd4d7cb73ca08ee500a21e73c60b94e500ff4ef8
sadgedog <sorachi1231.214@gmail.com>
2022-11-27 17:20:41 +0900

    Support JPEG

--- src/main.py
+++ src/main.py
@@ -23,6 +23,8 @@ from const import (
 from util import (
     encoder_bmp,
     decoder_bmp,
+    encoder_jpeg,
+    decoder_jpeg,
 )
 
 def show_img(image):
@@ -38,24 +40,66 @@ def show_img(image):
     # window size
     W = width * 5
     H = height * 5
+    W2 = width * 8
+    H2 = height * 8
+
     for i in range(len(all_image)):
         # resize window
         all_image[i] = cv2.resize(all_image[i], (W, H))
-        
+        # BMP
         if i == 0:
             cv2.imshow("Original Image", all_image[i])
             cv2.moveWindow("Original Image", i, i)
         elif i == len(all_image) - 1:
             cv2.imshow("Recovered Image", all_image[i])
             cv2.moveWindow("Recovered Image", i * W, 0)
+        # JPEG SHOW
+        # elif i == len(all_image) - 2:
+        #     all_image[i] = cv2.resize(all_image[i], (W2, H2))
+        #     cv2.imshow("Original JPEG Image", all_image[i])
+        #     cv2.moveWindow("Original JPEG Image", 0, 400)
+        # elif i == len(all_image) - 1:
+        #     all_image[i] = cv2.resize(all_image[i], (W2, H2))
+        #     cv2.imshow("Recovered JPEG Image", all_image[i])
+        #     cv2.moveWindow("Recovered JPEG Image", W2, 400)
+        # BMP
         else:
             title = "Share" + str(i)
             cv2.imshow(title, all_image[i])
             cv2.moveWindow(title, i * W, 0)
+    
+    cv2.waitKey(1)
+    # cv2.destroyAllWindows()
+
+
+def show_img2(image):
+    all_image = []
+    for img in image:
+        all_image.append(cv2.imread(img))
+
+    W = width * 8
+    H = height * 8
+
+    for i in range(len(all_image)):
+        # resize window
+        all_image[i] = cv2.resize(all_image[i], (W, H))
+        # BMP
+        if i == 0:
+            cv2.imshow("Original JPEG Image", all_image[i])
+            cv2.moveWindow("Original JPEG Image", 0, 400)
+        elif i == len(all_image) - 1:
+            cv2.imshow("Recovered JPEG Image", all_image[i])
+            cv2.moveWindow("Recovered JPEG Image", W, 400)
+
+        else:
+            title = "Share" + str(i)
+            cv2.imshow(title, all_image[i])
+            cv2.moveWindow(title, i * W, 0)    
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
+    
 def main():
     args = sys.argv
     if len(args) == 1:
@@ -64,9 +108,9 @@ def main():
     else:
         n = int(args[1])
         k = int(args[2])
-
     
     Secret = encoder_bmp(gopher)
+    
     shares = generate_share(Secret, n, k)
     for i in range(len(shares)):
         print(f"share[{i}] : ", shares[i])
@@ -84,7 +128,7 @@ def main():
     print("Original Data :  ", Secret)
     print("Recovered Data : ", recovered_img)
     if Secret == recovered_img:
-         print("Correctly Recovered!!")
+         print("Correctly Recovered BMP IMAGE!!")
     else:
         print("Recover Failed!!")
 
@@ -94,6 +138,47 @@ def main():
     l.append(REC + "Recovered_Image.bmp")
         
     show_img(l)
+
+    # JPEG TEST
+    print("JPEG TEST START")
+    Secret = encoder_jpeg(image_path)
+    Shares = []
+    for i in range(len(Secret)):
+        tmp = generate_share(Secret[i], n, k)
+        Shares.append(tmp)
+        
+    for i in range(len(Shares)):
+        print(f"share[{i}]", Shares[i])
+        
+    print("Generate Shares completed")
+    
+    recovered_img = []
+    for i in range(len(Shares)):
+        tmp = lagrange(0, Shares[i])
+        recovered_img.append(tmp)
+        print("recovered_img", tmp)
+        
+    print("Recovered Image completed")
+    
+    # write recovered image
+    decoder_jpeg(REC, "Recovered_JPEG_Image.jpg", recovered_img)
+    print("decode Image completed")
+
+    # check image
+    for i in range(len(Secret)):
+        recovered_img[i] = int(recovered_img[i], 16)
+        if Secret[i] == recovered_img[i]:
+            pass
+        else:
+            print("Recover Failed!!")
+            exit(1)
+    print("Correctly Recovered JPEG IMAGE!!")
+
+    # l.append(image_path)
+    # l.append(REC + "Recovered_JPEG_Image.jpg")
+    l = [image_path]
+    l.append(REC + "Recovered_JPEG_Image.jpg")
+    show_img2(l)
 
 
 if __name__ == "__main__":
--- src/util.py
+++ src/util.py
@@ -1,3 +1,6 @@
+import sys
+sys.set_int_max_str_digits(1000000000)
+
 import cv2
 from const import (
     image_path,
@@ -12,17 +15,53 @@ def encoder(img: str) -> str:
     # cv2.imshow("img", image)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-    r_flg, encoded_img = cv2.imencode('.bmp', image)
+    r_flg, encoded_img = cv2.imencode('.jpeg', image)
 
     return encoded_img
 
+# int, str間の変換は桁数が大きくなるとかなり遅い
+# そのためhexのまま分割してその先でint変換する
+def encoder_jpeg(img: str) -> list[int]:
+    # img = encoder(img)
+    with open(img, "rb") as f:
+        f = f.read().hex()
+        # f = int(f, 16)
+    img = f
+    encoded_img = []
+    # integer limit 4300 in python
+    # for each elemet -> 1000 * (1 ~ 3)
+    # img = str(img)
+    for i in range(0, len(img), 1000):
+        tmp = "".join(map(str, img[i : i + 1000]))
+        tmp = int(tmp, 16)
+        encoded_img.append(tmp)
+
+    return encoded_img
+
+
+def decoder_jpeg(filepath: str, name, img: list) -> None:
+    # 16 -> 10 -> 16で桁が消えることがあるので
+    # 桁数合わせて0パディング
+    for i in range(len(img)):
+        img[i] = hex(int(img[i]))[2:]
+        while len(img[i]) % 1000 != 0 and img.index(img[-1]) != i:
+            img[i] = "0" + img[i]
+    data = "".join(map(str, img))
+    data = bytes.fromhex(data)
+    
+    filename = filepath + name
+    with open(filename, "wb") as f:
+        f.write(data)
+
+# decoder_jpeg("../recovered_image/", "test.jpeg", Result)
+    
 
 def encoder_bmp(img: str) -> int:
     with open(img, "rb") as f:
         # enc_img = f.read()
         # enc_img = int.from_bytes(enc_img, byteorder = "big")
         enc_img = f.read().hex()
-        # print(enc_img)
+        print(enc_img)
         enc_img = enc_img[len(fmt):]
         enc_img = int(enc_img, 16)
         
